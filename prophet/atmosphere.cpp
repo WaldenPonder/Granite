402ef6c9975af7db56f91d6978f4112889c93e9a
wangstu <wangqianstu@163.com>
2023-06-23 23:47:16 +0800

    -

--- prophet/atmosphere.cpp
+++ prophet/atmosphere.cpp
@@ -2,6 +2,7 @@
 #include "render_graph.hpp"
 #include "renderer.hpp"
 
+using namespace Granite;
 AtmosphereParameters push;
 UBO ubo;
 
@@ -94,4 +95,279 @@ void setup_atmosphere(Granite::RenderGraph &graph, Renderer& renderer, RenderQue
 		    return true;
 	    });
 	graph.set_backbuffer_source("RayMarching");
+}
+
+
+
+struct TexturePlaneInfo
+{
+	Vulkan::Program *program;
+	const Vulkan::ImageView *reflection;
+	const Vulkan::ImageView *refraction;
+	const Vulkan::ImageView *normal;
+
+	struct Push
+	{
+		vec4 normal;
+		vec4 tangent;
+		vec4 bitangent;
+		vec4 position;
+		vec4 dPdx;
+		vec4 dPdy;
+		vec4 offset_scale;
+		vec4 base_emissive;
+	};
+	Push push;
+};
+
+static void texture_plane_render(CommandBuffer &cmd, const RenderQueueData *infos, unsigned instances)
+{
+	for (unsigned i = 0; i < instances; i++)
+	{
+		auto &info = *static_cast<const TexturePlaneInfo *>(infos[i].render_info);
+		cmd.set_program(info.program);
+		if (info.reflection)
+			cmd.set_texture(2, 0, *info.reflection, Vulkan::StockSampler::DefaultGeometryFilterClamp);
+		if (info.refraction)
+			cmd.set_texture(2, 1, *info.refraction, Vulkan::StockSampler::DefaultGeometryFilterClamp);
+		cmd.set_texture(2, 2, *info.normal, Vulkan::StockSampler::DefaultGeometryFilterWrap);
+		CommandBufferUtil::set_quad_vertex_state(cmd);
+		cmd.set_cull_mode(VK_CULL_MODE_NONE);
+		cmd.push_constants(&info.push, 0, sizeof(info.push));
+		CommandBufferUtil::draw_quad(cmd);
+	}
+}
+
+TexturePlane2::TexturePlane2(const std::string &normal_path)
+{
+	normalmap =
+	    GRANITE_ASSET_MANAGER()->register_image_resource(*GRANITE_FILESYSTEM(), normal_path, ImageClass::Normal);
+}
+
+void TexturePlane2::setup_render_pass_resources(RenderGraph &graph)
+{
+	reflection = nullptr;
+	refraction = nullptr;
+
+	if (need_reflection)
+		reflection =
+		    &graph.get_physical_texture_resource(graph.get_texture_resource(reflection_name).get_physical_index());
+	if (need_refraction)
+		refraction =
+		    &graph.get_physical_texture_resource(graph.get_texture_resource(refraction_name).get_physical_index());
+}
+
+void TexturePlane2::setup_render_pass_dependencies(RenderGraph &, Granite::RenderPass &target,
+                                                  RenderPassCreator::DependencyFlags dep_type)
+{
+	if ((dep_type & RenderPassCreator::MATERIAL_BIT) != 0)
+	{
+		if (need_reflection)
+			target.add_texture_input(reflection_name);
+		if (need_refraction)
+			target.add_texture_input(refraction_name);
+	}
+}
+
+void TexturePlane2::setup_render_pass_dependencies(RenderGraph &)
+{
+}
+
+void TexturePlane2::set_scene(Scene *scene_)
+{
+	scene = scene_;
+}
+
+void TexturePlane2::render_main_pass(Vulkan::CommandBuffer &cmd, const mat4 &proj, const mat4 &view)
+{
+	LightingParameters lighting = *base_context->get_lighting_parameters();
+	lighting.shadows = nullptr;
+	lighting.cluster = nullptr;
+
+	context.set_lighting_parameters(&lighting);
+	context.set_camera(proj, view);
+
+	visible.clear();
+	scene->gather_visible_opaque_renderables(context.get_visibility_frustum(), visible);
+	scene->gather_visible_transparent_renderables(context.get_visibility_frustum(), visible);
+	scene->gather_unbounded_renderables(visible);
+
+	// FIXME: Need to rethink this. We shouldn't be allowed to mutate the renderer suite.
+	LOGE("FIXME, TexturePlane2::render_main_pass\n");
+	auto &renderer = renderer_suite->get_renderer(RendererSuite::Type::ForwardOpaque);
+	//renderer.set_mesh_renderer_options_from_lighting(lighting);
+	renderer.begin(internal_queue);
+	internal_queue.push_renderables(context, visible.data(), visible.size());
+	renderer.flush(cmd, internal_queue, context);
+}
+
+void TexturePlane2::set_plane(const vec3 &position_, const vec3 &normal_, const vec3 &up_, float extent_up,
+                             float extent_across)
+{
+	position = position_;
+	normal = normal_;
+	up = up_;
+	rad_up = extent_up;
+	rad_x = extent_across;
+
+	dpdx = normalize(cross(normal, up)) * extent_across;
+	dpdy = normalize(up) * -extent_up;
+}
+
+void TexturePlane2::set_zfar(float zfar_)
+{
+	zfar = zfar_;
+}
+
+void TexturePlane2::add_render_pass(RenderGraph &graph, Type type)
+{
+	auto &device = graph.get_device();
+	bool supports_32bpp =
+	    device.image_format_is_supported(VK_FORMAT_B10G11R11_UFLOAT_PACK32, VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT);
+
+	AttachmentInfo color, depth, reflection_blur;
+	color.format = supports_32bpp ? VK_FORMAT_B10G11R11_UFLOAT_PACK32 : VK_FORMAT_R16G16B16A16_SFLOAT;
+	depth.format = device.get_default_depth_format();
+
+	color.size_x = scale_x;
+	color.size_y = scale_y;
+	depth.size_x = scale_x;
+	depth.size_y = scale_y;
+
+	reflection_blur.size_x = 0.5f * scale_x;
+	reflection_blur.size_y = 0.5f * scale_y;
+	reflection_blur.levels = 0;
+	reflection_blur.flags |= ATTACHMENT_INFO_MIPGEN_BIT;
+
+	auto &name = type == Reflection ? reflection_name : refraction_name;
+
+	auto &lighting = graph.add_pass(name + "-lighting", RENDER_GRAPH_QUEUE_GRAPHICS_BIT);
+	lighting.add_color_output(name + "-HDR", color);
+	lighting.set_depth_stencil_output(name + "-depth", depth);
+
+	lighting.set_get_clear_depth_stencil(
+	    [](VkClearDepthStencilValue *value) -> bool
+	    {
+		    if (value)
+		    {
+			    value->depth = 1.0f;
+			    value->stencil = 0;
+		    }
+		    return true;
+	    });
+
+	lighting.set_get_clear_color(
+	    [](unsigned, VkClearColorValue *value) -> bool
+	    {
+		    if (value)
+			    memset(value, 0, sizeof(*value));
+		    return true;
+	    });
+
+	lighting.set_build_render_pass(
+	    [this, type](Vulkan::CommandBuffer &cmd)
+	    {
+		    if (type == Reflection)
+		    {
+			    mat4 proj, view;
+			    float z_near;
+			    compute_plane_reflection(proj, view, base_context->get_render_parameters().camera_position, position,
+			                             normal, up, rad_up, rad_x, z_near, zfar);
+
+			    // FIXME: Should not be allowed.
+			    LOGE("FIXME, TexturePlane2::add_render_pass\n");
+			    //renderer.set_mesh_renderer_options(Renderer::ENVIRONMENT_ENABLE_BIT | Renderer::SHADOW_ENABLE_BIT);
+
+			    if (zfar > z_near)
+				    render_main_pass(cmd, proj, view);
+		    }
+		    else if (type == Refraction)
+		    {
+			    mat4 proj, view;
+			    float z_near;
+			    compute_plane_refraction(proj, view, base_context->get_render_parameters().camera_position, position,
+			                             normal, up, rad_up, rad_x, z_near, zfar);
+
+			    // FIXME: Should not be allowed.
+			    //renderer.set_mesh_renderer_options(Renderer::ENVIRONMENT_ENABLE_BIT | Renderer::SHADOW_ENABLE_BIT | Renderer::REFRACTION_ENABLE_BIT);
+
+			    if (zfar > z_near)
+				    render_main_pass(cmd, proj, view);
+		    }
+	    });
+
+	lighting.add_texture_input("shadow-main");
+
+	auto &reflection_blur_pass = graph.add_pass(name, RENDER_GRAPH_QUEUE_GRAPHICS_BIT);
+	auto &reflection_input_res = reflection_blur_pass.add_texture_input(name + "-HDR");
+	reflection_blur_pass.add_color_output(name, reflection_blur);
+	reflection_blur_pass.set_build_render_pass(
+	    [&](Vulkan::CommandBuffer &cmd)
+	    {
+		    cmd.set_texture(0, 0, graph.get_physical_texture_resource(reflection_input_res),
+		                    Vulkan::StockSampler::LinearClamp);
+		    CommandBufferUtil::draw_fullscreen_quad(cmd, "builtin://shaders/quad.vert", "builtin://shaders/blur.frag",
+		                                            { { "METHOD", 6 } });
+	    });
+}
+
+void TexturePlane2::add_render_passes(RenderGraph &graph)
+{
+	if (need_reflection)
+		add_render_pass(graph, Reflection);
+	if (need_refraction)
+		add_render_pass(graph, Refraction);
+}
+
+void TexturePlane2::set_base_renderer(const RendererSuite *suite)
+{
+	renderer_suite = suite;
+}
+
+void TexturePlane2::set_base_render_context(const RenderContext *context_)
+{
+	base_context = context_;
+}
+
+void TexturePlane2::get_render_info(const RenderContext &context_, const RenderInfoComponent *, RenderQueue &queue) const
+{
+	TexturePlaneInfo info;
+	info.reflection = reflection;
+	info.refraction = refraction;
+	info.normal = queue.get_resource_manager().get_image_view(normalmap);
+	info.push.normal = vec4(normalize(normal), 0.0f);
+	info.push.position = vec4(position, 0.0f);
+	info.push.dPdx = vec4(dpdx, 0.0f);
+	info.push.dPdy = vec4(dpdy, 0.0f);
+	info.push.tangent = vec4(normalize(dpdx), 0.0f);
+	info.push.bitangent = vec4(normalize(dpdy), 0.0f);
+	info.push.offset_scale = vec4(vec2(0.03 * base_context->get_frame_parameters().elapsed_time), vec2(2.0f));
+	info.push.base_emissive = vec4(base_emissive, 0.0f);
+
+	Util::Hasher h;
+	if (info.reflection)
+		h.u64(info.reflection->get_cookie());
+	else
+		h.u32(0);
+
+	if (info.refraction)
+		h.u64(info.refraction->get_cookie());
+	else
+		h.u32(0);
+
+	h.u64(info.normal->get_cookie());
+	auto instance_key = h.get();
+	auto sorting_key = RenderInfo::get_sort_key(context_, Queue::OpaqueEmissive, h.get(), h.get(), position);
+	auto *plane_info =
+	    queue.push<TexturePlaneInfo>(Queue::OpaqueEmissive, instance_key, sorting_key, texture_plane_render, nullptr);
+
+	if (plane_info)
+	{
+		unsigned mat_mask = MATERIAL_EMISSIVE_BIT;
+		mat_mask |= info.refraction ? MATERIAL_EMISSIVE_REFRACTION_BIT : 0;
+		mat_mask |= info.reflection ? MATERIAL_EMISSIVE_REFLECTION_BIT : 0;
+		info.program = queue.get_shader_suites()[Util::ecast(RenderableType::TexturePlane)].get_program(
+		    VariantSignatureKey::build(DrawPipeline::Opaque, 0, mat_mask));
+		*plane_info = info;
+	}
 }
--- prophet/main.cpp
+++ prophet/main.cpp
@@ -1,7 +1,10 @@
 #include "atmosphere.h"
 #include "flat_renderer.hpp"
+#include "path.hpp"
 #include "scene_loader.hpp"
+#include "ui_manager.hpp"
 
+#if 0
 struct TestRenderGraph : Granite::Application, Granite::EventHandler
 {
 	TestRenderGraph()
@@ -128,6 +131,159 @@ Application *application_create(int, char **)
 	try
 	{
 		auto *app = new TestRenderGraph();
+		return app;
+	}
+	catch (const std::exception &e)
+	{
+		LOGE("application_create() threw exception: %s\n", e.what());
+		return nullptr;
+	}
+}
+} // namespace Granite
+#endif
+
+struct ViewerApplication : Application
+{
+	explicit ViewerApplication(const std::string &path)
+	    : renderer(RendererType::GeneralForward, /* resolver */ nullptr)
+	{
+		// Using Renderer directly is somewhat low level.
+		// Normally, you would use RendererSuite and RenderPassSceneRenderer.
+
+		// Effectively, loads a scene and inserts Entity objects into the Scene.
+		scene_loader.load_scene(path);
+
+		// Set initial position.
+		fps_camera.set_position(vec3(0.0f, 0.0f, 5.0f));
+		fps_camera.set_depth_range(0.1f, 500.0f);
+
+		auto &scene = scene_loader.get_scene();
+
+		auto plane = Util::make_handle<TexturePlane2>("test_normal");
+
+		plane->set_plane(vec3(0, 0, 0), vec3(0, 0, 1), vec3(0, 0, 1), 1, 1);
+		plane->set_zfar(500.f);
+		plane->set_reflection_name("reflectionName");
+
+		auto entity = scene.create_renderable(plane, nullptr);
+		entity->allocate_component<UnboundedComponent>();
+		entity->allocate_component<RenderPassSinkComponent>();
+		auto *cull_plane = entity->allocate_component<CullPlaneComponent>();
+		cull_plane->plane = plane->get_plane();
+
+		auto *rpass = entity->allocate_component<RenderPassComponent>();
+		rpass->creator = plane.get();
+	}
+
+	void render_frame(double frame_time, double elapsed_time) override
+	{
+		// Simple serial variant.
+		auto &scene = scene_loader.get_scene();
+
+		// First, update game objects. Can modify their scene Node transforms.
+		// Objects can be added as well.
+		// Animation system could run here.
+		scene_loader.get_animation_system().animate(frame_time, elapsed_time);
+
+		// - Traverse the node hierarchy and compute full transform.
+		// - Updates the model and skinning matrices.
+		scene.update_all_transforms();
+
+		// Update the rendering context.
+		// Only use a single directional light.
+		// No shadows or anything fancy is used.
+		lighting.directional.color = vec3(1.0f, 0.9f, 0.8f);
+		lighting.directional.direction = normalize(vec3(1.0f, 1.0f, 1.0f));
+		context.set_lighting_parameters(&lighting);
+
+		// The renderer can be configured to handle many different scenarios.
+		// Here we reconfigure the renderer to work with the current lighting configuration.
+		// This is particularly necessary for forward renderers.
+		// For G-buffer renderers, only a few flags are relevant. This is handled automatically
+		// by the more advanced APIs such as RendererSuite and the RenderPassSceneRenderer.
+		renderer.set_mesh_renderer_options_from_lighting(lighting);
+
+		// The FPS camera registers for input events.
+		// Update all rendering matrices based on current Camera state.
+		// It is possible to pass down explicit matrices as well.
+		context.set_camera(fps_camera);
+
+		// Simple forward renderer, so we render opaque, transparent and background renderables in one go.
+		visible.clear();
+		scene.gather_visible_opaque_renderables(context.get_visibility_frustum(), visible);
+		scene.gather_visible_transparent_renderables(context.get_visibility_frustum(), visible);
+		scene.gather_unbounded_renderables(visible);
+
+		// Time to render.
+		renderer.begin(queue);
+		queue.push_renderables(context, visible.data(), visible.size());
+
+		auto &device = get_wsi().get_device();
+		auto cmd = device.request_command_buffer();
+		auto rp = device.get_swapchain_render_pass(SwapchainRenderPass::Depth);
+		rp.clear_color[0].float32[0] = 0.01f;
+		rp.clear_color[0].float32[1] = 0.02f;
+		rp.clear_color[0].float32[2] = 0.03f;
+		cmd->begin_render_pass(rp);
+		renderer.flush(*cmd, queue, context, 0, nullptr);
+
+		// Render some basic 2D on top.
+		flat_renderer.begin();
+
+		flat_renderer.render_text(GRANITE_UI_MANAGER()->get_font(UI::FontSize::Large), "Hello Granite",
+		                          vec3(10.0f, 10.0f, 0.0f), vec2(1000.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f));
+
+		// The camera_pos and camera_size denote the canvas size.
+		// We work in pixel units mostly, so using the viewport size as a baseline is a good default.
+		// The Z dimension denotes how we subdivide the depth plane.
+		// 2D objects also have depth and make use of the depth buffer (opaque 2D objects).
+		flat_renderer.flush(*cmd, vec3(0.0f), vec3(cmd->get_viewport().width, cmd->get_viewport().height, 1.0f));
+
+		cmd->end_render_pass();
+		device.submit(cmd);
+	}
+
+	// Modify these as desired. For now, just call into the parent,
+	// so it's effectively the same as not overriding.
+	// This code is only here for demonstration purposes.
+	void post_frame() override
+	{
+		Application::post_frame();
+	}
+
+	void render_early_loading(double frame_time, double elapsed_time) override
+	{
+		Application::render_early_loading(frame_time, elapsed_time);
+	}
+
+	void render_loading(double frame_time, double elapsed_time) override
+	{
+		Application::render_loading(frame_time, elapsed_time);
+	}
+
+	FPSCamera fps_camera;
+	RenderContext context;
+	LightingParameters lighting = {};
+	SceneLoader scene_loader;
+	FlatRenderer flat_renderer;
+	Renderer renderer;
+	RenderQueue queue;
+	VisibilityList visible;
+};
+
+namespace Granite
+{
+Application *application_create(int argc, char **argv)
+{
+	GRANITE_APPLICATION_SETUP_FILESYSTEM();
+
+	std::string path = "J:/Scene/plane.glb";
+
+	LOGI("Loading glTF file from %s.\n", path.c_str());
+
+	try
+	{
+		auto *app = new ViewerApplication(path);
 		return app;
 	}
 	catch (const std::exception &e)
